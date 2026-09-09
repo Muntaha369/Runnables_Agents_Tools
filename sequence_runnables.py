b5d7f9f46f9e9d1d49e79af4eeda19de2a328fda
Muntaha <muntahasayed396@gmail.com>
2026-09-10 00:26:44 +0530

    seq_runnables

---
+++ sequence_runnables.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from langchain_openrouter import ChatOpenRouter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+model = ChatOpenRouter(model="gpt-4o-mini")
+
+parser = StrOutputParser()
+
+short_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in 1-2 lines"
+)
+
+pipeline = short_prompt | model | parser 
+
+result = pipeline.invoke({"topic":"ML"})
+
+print(result)
